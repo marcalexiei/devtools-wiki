e32c7a06dc7e94be02f94ed8e24fefcd29dcdda3
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-07-18 23:04:41 +0200

    feat: add 404 page

---
+++ src/components/App/AppPage404.tsx
@@ -0,0 +1,13 @@
+import { UIPageLayout } from '../UIPage/UIPageLayout';
+
+export function AppPage404() {
+  return (
+    <UIPageLayout>
+      <h1>404 - Not found</h1>
+
+      <nav>
+        <a href="/">Return to home</a>
+      </nav>
+    </UIPageLayout>
+  );
+}
