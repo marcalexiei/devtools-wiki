c9307610ca4f700f14a3b3edc85a702882d461a1
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-08-24 14:32:37 +0200

    cd(vite.config): add `base`

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 // https://vitejs.dev/config/
 // biome-ignore lint/style/noDefaultExport: vite requires default export
 export default defineConfig({
+  base: '/devtools-wiki/',
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
