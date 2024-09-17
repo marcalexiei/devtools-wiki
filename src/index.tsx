3da8617f12489b60f9b01f56a370bc1aafde970f
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-09-17 18:03:38 +0200

    fix: customise 404 page to redirect to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,24 @@ import { ErrorBoundary, lazy, Suspense } from 'solid-js';
 import { Router, Route, Navigate } from '@solidjs/router';
 import { QueryClientProvider } from '@tanstack/solid-query';
 import { App } from './App';
-import { APP_BASE_PATH } from './AppBasePath';
 import { AppQueryClient } from './AppQueryClient';
 import { AppErrorBoundaryFallback } from './components/App/AppErrorBoundaryFallback';
 import { AppPage404 } from './components/App/AppPage404';
 import { ThemeContextProvider } from './components/Theme/ThemeContext';
 import { UIPageLayoutLoading } from './components/UIPage/UIPageLayoutLoading';
+import { APP_BASE_PATH } from './AppBasePath';
 
 import './style.scss';
+
+if (window.location.search.startsWith('?/')) {
+  const path = window.location.search.slice(1);
+  window.history.replaceState('redirect', '', `/${APP_BASE_PATH}${path}`);
+}
 
 const Home = lazy(() =>
   import('./pages/Home/Home').then(({ Home: Cmp }) => ({ default: Cmp })),
 );
+
 const Outgoing = lazy(() =>
   import('./pages/Outgoing/Outgoing').then(({ Outgoing: Cmp }) => ({
     default: Cmp,
